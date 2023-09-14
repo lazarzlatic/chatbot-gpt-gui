01ec7387c1ec45afee4432c065ec3cd9763ebbba
lazarzlatic <lazar.zlatic@gmail.com>
2023-09-14 11:25:57 +0200

    Chatbot class, added get_response method

--- backend.py
+++ backend.py
@@ -1,2 +1,15 @@
+import openai
+
+
 class Chatbot:
-    pass
+    def __init__(self):
+        openai.api_key = "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"
+
+    def get_response(self, user_input):
+        response = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=user_input,
+            max_tokens=3000,
+            temperature=0.5
+        ).choice[0].text
+        return response
