dc600c8567033ddeccf803d3ec00444799f46a2d
lazarzlatic <lazar.zlatic@gmail.com>
2023-09-14 12:00:22 +0200

    Linking backend.py with main.py, input field type fix.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 
-from PyQt6.QtWidgets import QApplication, QMainWindow, QTextEdit, QPushButton
+from PyQt6.QtWidgets import QApplication, QMainWindow, QTextEdit, QPushButton, QLineEdit
+from backend import Chatbot
 
 
 class ChatbotWindow(QMainWindow):
@@ -17,14 +18,22 @@ class ChatbotWindow(QMainWindow):
         self.chat_area.setReadOnly(True)
 
         # Add the input field widget
-        self.input_field = QTextEdit(self)
+        self.input_field = QLineEdit(self)
         self.input_field.setGeometry(10, 340, 480, 40)
 
         # Add the button
         self.button = QPushButton("Send", self)
         self.button.setGeometry(500, 340, 100, 40)
+        self.button.clicked.connect(self.send_message)
 
         self.show()
+
+    def send_message(self):
+        user_input = self.input_field.text().strip()
+        self.chat_area.append(f"Me: {user_input}")
+        self.input_field.clear()
+
+        chatbot = Chatbot()
 
 
 app = QApplication(sys.argv)
